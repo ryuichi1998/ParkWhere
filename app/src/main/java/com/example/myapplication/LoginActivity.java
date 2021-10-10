e0257ecc7381791f6214105b17ebf2357f68de22
Ryu <ryuichi.chua@gmail.com>
2021-10-11 02:11:52 +0800

    Login Activity Login Fragment Register Fragment

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginBtn = (Button) findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ui/register/RegisterViewModel.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.ui.register;
+
+import androidx.lifecycle.ViewModel;
+
+public class RegisterViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
