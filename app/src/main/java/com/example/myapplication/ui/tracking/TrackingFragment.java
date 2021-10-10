2997e9ab473303b7b2591a01055631ae20e51b4e
ArmandXiao <xiao_lingao@outlook.com>
2021-10-11 01:25:23 +0800

    Restructure the onclick listener by implementing View.OnClickListener interface

--- app/src/main/java/com/example/myapplication/ui/tracking/TrackingFragment.java
+++ app/src/main/java/com/example/myapplication/ui/tracking/TrackingFragment.java
@@ -16,12 +16,11 @@ import android.widget.TextView;
 
 import com.example.myapplication.R;
 
-public class TrackingFragment extends Fragment {
+public class TrackingFragment extends Fragment implements View.OnClickListener{
     private TrackingViewModel mViewModel;
 
     private View root;
     private Button btn_tracker;
-    private TextView txtv3;
 
 
     public static TrackingFragment newInstance() {
@@ -33,23 +32,21 @@ public class TrackingFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.tracking_fragment, container, false);
         btn_tracker = root.findViewById(R.id.btn_tracker);
-        txtv3 = root.findViewById(R.id.textView3);
-        btn_tracker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                txtv3.setText("Hello");
-
-            }
-        });
+        btn_tracker.setOnClickListener(this);   // Important to add this listener
 
         return root;
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(TrackingViewModel.class);
-        // TODO: Use the ViewModel
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_tracker:
+                doSomething();
+        }
     }
 
+    private void doSomething() {
+        TextView txtv3 = root.findViewById(R.id.textView3);
+        txtv3.setText("New Attempt");
+    }
 }
